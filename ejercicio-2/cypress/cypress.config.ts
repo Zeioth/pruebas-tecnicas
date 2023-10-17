0cf1040438fae3d4a9f6521609b6af6569eda80f
Zeioth <zeioth@hotmail.com>
2023-10-18 00:35:05 +0200

    feat: ejercicio 2

---
+++ ejercicio-2/cypress/cypress.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'cypress';
+import { mergeConfigWithConfigFromFile } from './cypress/config/setup-node-events/mergeConfigWithConfigFromFile';
+import { plugins } from 'cypress/config/setup-node-events/plugins';
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 30000,
+  pageLoadTimeout: 60000,
+  projectId: 'iyhpy5',
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'cypress/config/reporter-config.json'
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0
+  },
+  screenshotsFolder: 'cypress/screenshots',
+  video: false,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  watchForFileChanges: false,
+  e2e: {
+    supportFile: false,
+    testIsolation: true,
+    setupNodeEvents(on, config) {
+      const newConfig = mergeConfigWithConfigFromFile(config);
+      plugins(on);
+      console.info('\n> Cypress config:\n', newConfig);
+      return newConfig;
+    },
+    specPattern: 'cypress/e2e/**/*.ts'
+  }
+});
