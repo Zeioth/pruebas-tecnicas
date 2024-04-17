4fc24aa7109a79ad46c17a65789a15a9d53d186a
Zeioth <zeioth@hotmail.com>
2024-04-17 22:19:53 +0200

    refactor: Let's not use the type 'any' + format.

--- ejercicio-3/src/services/logger.service.ts
+++ ejercicio-3/src/services/logger.service.ts
@@ -6,34 +6,34 @@ const logsDir = 'logs'
 const logFileName = 'logfile.log'
 const logFilePath = path.join(logsDir, logFileName)
 
-// ensure logsDir is created
+// Ensure logsDir is created.
 fs.mkdirSync(logsDir, { recursive: true })
 
-// define the log format
+// Define the log format.
 const opts = {
   level: process.env.PINO_LOG_LEVEL || 'info',
   formatters: {
-    bindings: () => { return {} }, // note we are silencing bindings.
-    level: (label: any) => {
-      return { level: label.toUpperCase() };
+    bindings: () => { return {} }, // Note we are silencing bindings.
+    level: (label: string) => {
+      return { level: label.toUpperCase() }
     },
   },
   timestamp: () => {
-    const date = new Date();
+    const date = new Date()
     const locale = 'es-ES'
     const dateString = date.toLocaleDateString(
       locale, { day: '2-digit', month: '2-digit', year: 'numeric' }
-    );
-    const timeString = date.toLocaleTimeString(locale, { hour12: false });
+    )
+    const timeString = date.toLocaleTimeString(locale, { hour12: false })
 
-    // Join 'date' and 'time' into the final 'timestamp' and return it
-    const timestamp = `,"timestamp":"${dateString} at ${timeString}"`;
+    // Join 'date' and 'time' into the final 'timestamp' and return it.
+    const timestamp = `,"timestamp":"${dateString} at ${timeString}"`
     return timestamp
   },
 }
 
-// setup logger
-const stream = fs.createWriteStream(logFilePath, { flags: 'a' }) // append log.
+// Setup logger
+const stream = fs.createWriteStream(logFilePath, { flags: 'a' }) // Append log.
 const logger = pino(opts, stream)
 
 export default logger
