606ae98a6c8fa4b432e993003a3b347b502d8d2c
Zeioth <zeioth@hotmail.com>
2024-04-17 22:34:00 +0200

    refactor(logger): Stronger type checkings.

--- ejercicio-3/src/services/logger.service.ts
+++ ejercicio-3/src/services/logger.service.ts
@@ -1,16 +1,20 @@
-import pino from 'pino'
+import pino, { LoggerOptions } from 'pino'
 import path from 'path'
 import fs from 'fs'
 
-const logsDir = 'logs'
-const logFileName = 'logfile.log'
-const logFilePath = path.join(logsDir, logFileName)
+const logsDir: string = 'logs'
+const logFileName: string = 'logfile.log'
+const logFilePath: string = path.join(logsDir, logFileName)
 
-// Ensure logsDir is created.
+// Ensure 'logsDir' is created and open the stream.
 fs.mkdirSync(logsDir, { recursive: true })
+const stream:fs.WriteStream = fs.createWriteStream(
+  logFilePath,
+  { flags: 'a' } // 'a' for 'append' instead of re-creating the log per session.
+)
 
 // Define the log format.
-const opts = {
+const opts: LoggerOptions = {
   level: process.env.PINO_LOG_LEVEL || 'info',
   formatters: {
     bindings: () => { return {} }, // Note we are silencing bindings.
@@ -19,21 +23,21 @@ const opts = {
     },
   },
   timestamp: () => {
-    const date = new Date()
-    const locale = 'es-ES'
-    const dateString = date.toLocaleDateString(
+    const date: Date = new Date()
+    const locale: string = 'es-ES'
+    const dateString: string = date.toLocaleDateString(
       locale, { day: '2-digit', month: '2-digit', year: 'numeric' }
     )
-    const timeString = date.toLocaleTimeString(locale, { hour12: false })
+    const timeString: string = date.toLocaleTimeString(locale, { hour12: false })
 
     // Join 'date' and 'time' into the final 'timestamp' and return it.
-    const timestamp = `,"timestamp":"${dateString} at ${timeString}"`
+    const timestamp: string = `,"timestamp":"${dateString} at ${timeString}"`
     return timestamp
   },
 }
 
 // Setup logger
-const stream = fs.createWriteStream(logFilePath, { flags: 'a' }) // Append log.
-const logger = pino(opts, stream)
+const logger: pino.Logger = pino(opts, stream)
 
+/** Singleton of the logger. */
 export default logger
