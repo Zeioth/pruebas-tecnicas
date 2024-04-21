180324e2a86a3c1215a95d9eaf13e564cd088767
Zeioth <zeioth@hotmail.com>
2024-04-21 16:34:40 +0200

    fix(`tests/services/logger.service.ts`): Let's introduce a small delay. If this still not work, then the cause is the GitHub automated action has no storage, and therefore it failts to write.

--- ejercicio-3/tests/services/logger.service.spec.ts
+++ ejercicio-3/tests/services/logger.service.spec.ts
@@ -32,16 +32,20 @@ describe('logger', () => {
     logger.info(expectedLogs[0].msg)
     logger.warn(expectedLogs[1].msg)
     logger.error(expectedLogs[2].msg)
+    await new Promise(resolve => setTimeout(resolve, 1000))
+
     logger.flush(() => {
-      // assert → 'expectedLogs' are the latest lines written in the logs file
-      logContent = fs.readFileSync(logFilePath, 'utf8')
-      logContentLines = logContent.trim().split('\n')
-      last3LinesAsJson = logContentLines.slice(-3).map(
-        (line: string) => JSON.parse(line))
-      last3LinesAsJson.forEach((line: LogLine, index: number) => {
-        expect(line.level).toBe(expectedLogs[index].level)
-        expect(line.msg).toBe(expectedLogs[index].msg)
-      })
+      setTimeout(() => {
+        // assert → 'expectedLogs' are the latest lines written in the logs file
+        logContent = fs.readFileSync(logFilePath, 'utf8')
+        logContentLines = logContent.trim().split('\n')
+        last3LinesAsJson = logContentLines.slice(-3).map(
+          (line: string) => JSON.parse(line))
+        last3LinesAsJson.forEach((line: LogLine, index: number) => {
+          expect(line.level).toBe(expectedLogs[index].level)
+          expect(line.msg).toBe(expectedLogs[index].msg)
+        })
+      }, 1000)
     })
   })
 })
