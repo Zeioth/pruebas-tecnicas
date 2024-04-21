719cb48699d04ffc59a62a7daa35c6576e78e90a
Zeioth <zeioth@hotmail.com>
2024-04-21 20:29:49 +0200

    ✅test(pet controller): Now test the case of use where we instance the class instead of using the singleton (allowing this is a hard requirement for this technical interview)

--- ejercicio-3/tests/controllers/petController.spec.ts
+++ ejercicio-3/tests/controllers/petController.spec.ts
@@ -1,5 +1,5 @@
 import { Pet, PetsWithSameName, PetStatus } from '@interfaces/IPet'
-import { petController } from '@controllers/petController'
+import { petController, PetController } from '@controllers/petController'
 import { arrayOfPets1 } from '@fixtures/arrayOfPets'
 
 describe('petController', () => {
@@ -16,17 +16,43 @@ describe('petController', () => {
     expect(pets).toMatchObject<Pet[]>
   }),
 
-  it('should GET a list of pets by calling countPetsWithSameName() and expect a `PetsWithSameName[]` interface.', async () => {
-    // arrange
-    const pets: Pet[] = arrayOfPets1
-    let petsWithSameName: PetsWithSameName|null = null
+    it('should GET a list of pets by calling countPetsWithSameName() and expect a `PetsWithSameName[]` interface.', async () => {
+      // arrange
+      const pets: Pet[] = arrayOfPets1
+      let petsWithSameName: PetsWithSameName | null = null
 
-    // act
-    petsWithSameName = await petController.countPetsWithSameName(pets)
+      // act
+      petsWithSameName = await petController.countPetsWithSameName(pets)
 
-    // assert
-    expect(petsWithSameName).not.toBeUndefined
-    expect(petsWithSameName).not.toBeNull
-    expect(petsWithSameName).toMatchObject<PetsWithSameName[]>
-  })
+      // assert
+      expect(petsWithSameName).not.toBeUndefined
+      expect(petsWithSameName).not.toBeNull
+      expect(petsWithSameName).toMatchObject<PetsWithSameName[]>
+    }),
+
+    it('should GET pets and count the ones with the same name using the a controller class (hard requirement).', async () => {
+      // arrange
+      let pets: Pet[] | null = null
+      let pc: PetController
+      let pwsn: PetsWithSameName = {}
+
+      // act
+      pets = await petController.getPets(PetStatus.Sold)
+      if (pets) {
+        pc = new PetController(pets)
+        pc.setPetsProp = pets
+        pwsn = await pc.countPetsWithSameName(pc.getPetsProp)
+        console.log(pwsn)
+      }
+
+      // assert pets
+      expect(pets).not.toBeUndefined
+      expect(pets).not.toBeNull
+      expect(pets).toMatchObject<Pet[]>
+
+      // assert petsWithSameName
+      expect(pwsn).not.toBeUndefined
+      expect(pwsn).not.toBeNull
+      expect(pwsn).toMatchObject<PetsWithSameName[]>
+    })
 })
