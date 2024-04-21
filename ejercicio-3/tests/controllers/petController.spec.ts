5337a53c8e2ca0e6a68457e659e82cfac5e8e19e
Zeioth <zeioth@hotmail.com>
2024-04-21 20:32:31 +0200

    refactor: No need of console.log here for now.

--- ejercicio-3/tests/controllers/petController.spec.ts
+++ ejercicio-3/tests/controllers/petController.spec.ts
@@ -42,7 +42,6 @@ describe('petController', () => {
         pc = new PetController(pets)
         pc.setPetsProp = pets
         pwsn = await pc.countPetsWithSameName(pc.getPetsProp)
-        console.log(pwsn)
       }
 
       // assert pets
