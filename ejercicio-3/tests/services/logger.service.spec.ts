abd27906c83f87c7501262fbd026ee821c9ace3d
Zeioth <zeioth@hotmail.com>
2024-04-21 16:08:39 +0200

    ✅test(logger): logger.flush() should ensure the logs are writing sync for the test, while still working async for the rest of the program.

---
+++ ejercicio-3/tests/services/logger.service.spec.ts
@@ -0,0 +1,48 @@
+/** NOTE: This tests write into the actual log file.
+ *        If you don't want this behavior, comment this file.
+ *
+ *        - Mocking the tests won't ensure the service works as expected.
+ *        - Removing the content written by the service
+ *          would introduce unnecesary integrity risk. */
+
+import logger from '@services/logger.service'
+import fs from 'fs'
+import path from 'path'
+
+/** It represents the expected structure of a log line within this test file. */
+interface LogLine {
+  level: string
+  msg: string
+}
+
+describe('logger', () => {
+  it('should use the logger to write into the default log file correctly.', async () => {
+    // arrange
+    const logFilePath: string = path.resolve(__dirname, '../../logs/logfile.log')
+    const expectedLogs: LogLine[] = [
+      { "level": "INFO", "msg": "THIS IS A UNIT TEST." },
+      { "level": "WARN", "msg": "THIS IS A UNIT TEST." },
+      { "level": "ERROR", "msg": "THIS IS A UNIT TEST." },
+    ]
+    let logContent: string | null = null
+    let logContentLines: string[] | null = null
+    let last3LinesAsJson: LogLine[] | null = null
+
+    // act → log
+    logger.info(expectedLogs[0].msg)
+    logger.warn(expectedLogs[1].msg)
+    logger.error(expectedLogs[2].msg)
+    logger.flush() // force writing the logs on disk.
+
+    // assert → 'expectedLogs' are the latest lines written in the logs file
+    logContent = fs.readFileSync(logFilePath, 'utf8')
+    logContentLines = logContent.trim().split('\n')
+    last3LinesAsJson = logContentLines.slice(-3).map(
+      (line: string) => JSON.parse(line))
+    last3LinesAsJson.forEach((line: LogLine, index: number) => {
+      expect(line.level).toBe(expectedLogs[index].level)
+      expect(line.msg).toBe(expectedLogs[index].msg)
+    })
+  })
+})
+
