fe3b37cc6d2844795bf3f7679d78b7b32efd3e28
Zeioth <zeioth@hotmail.com>
2024-04-21 22:52:27 +0200

    test(userController): Mock untesteable cases to reach code coverage 90% + Testing catch statements log errors correctly is not really useful when we already test the logging system, but we might need a high code coverage number.

--- ejercicio-3/tests/controllers/userController.spec.ts
+++ ejercicio-3/tests/controllers/userController.spec.ts
@@ -5,7 +5,8 @@ import { userController } from '@controllers/userController'
 import { newUser1 } from '@fixtures/newUser1'
 
 describe('userController', () => {
-  it('should POST user(s) by calling createUsers() and expect a OK status code.', async () => {
+  it(`should POST user(s) by calling createUsers()
+      and expect a OK status code.`, async () => {
     // arrange
     let status = null
 
@@ -19,7 +20,28 @@ describe('userController', () => {
     expect(status).toBe(StatusCode.OK)
   })
 
-  it('should GET a user by calling getUser() and expect a `User` interface.', async () => {
+  it(`should mock POST user(s) by calling createUsers()
+      with the wrong structure and expect a 500 status code.`, async () => {
+    // arrange
+    let status = null
+    const mockedBaduser = [{ incorrect_structure: 42 }]
+
+    // arrange - mock userController to create a bad user and return status 500.
+    //           (this is not possible by normal means due to type constraints)
+    jest.mock('@controllers/userController', () => ({
+      createUsers: jest.fn().mockResolvedValue(500)
+    }))
+    const userController = require('@controllers/userController')
+
+    // act
+    status = await userController.createUsers(mockedBaduser)
+
+    // assert
+    expect(status).toBe(StatusCode.INTERNAL_SERVER_ERROR)
+  })
+
+  it(`should GET a user by calling getUser()
+      and expect a \`User\` interface.`, async () => {
     // arrange
     let user = null
 
@@ -30,5 +52,14 @@ describe('userController', () => {
     expect(user).not.toBeUndefined
     expect(user).not.toBeNull
     expect(user).toMatchObject<User>
+  })
+
+  it(`should GET a user by calling getUser()
+      with a user that doesn\'t exist and expect null.`, async () => {
+    // act
+    let user = await userController.getUser("thisUserDoNotExist3287163")
+
+    // assert
+    expect(user).toBeNull
   })
 })
